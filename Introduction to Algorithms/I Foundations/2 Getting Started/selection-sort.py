181fdba84521dd88b739462440dd1e96fd96f9ed
LoniasGR <avdelasleonidas@gmail.com>
2019-01-30 21:10:07 +0200

    Added selection sort

---
+++ Introduction to Algorithms/I Foundations/2 Getting Started/selection-sort.py
@@ -0,0 +1,13 @@
+def selectionsort (arr):
+    for i in range(0, len(arr)-1):
+        index = i
+        for j in range(i, len(arr)):
+            if arr[index] > arr[j]:
+                index = j
+        temp = arr[i]
+        arr[i] = arr[index]
+        arr[index] = temp
+    return arr
+
+# Test
+#print(selectionsort([5,10,1,2,3,5,5,9,231,52]))
